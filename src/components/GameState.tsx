9fbc3fcc83f63ca84f3b2a82acb1be8571c7a34a
Preeti <preetijav@gmail.com>
2021-01-13 18:54:17 +0100

    Bug/fix diagonal matrix scenario

--- src/components/GameState.tsx
+++ src/components/GameState.tsx
@@ -7,21 +7,22 @@ const createBoardState = () => Array<Value>(16).fill(null);
 
 function calculateWinner(boardState: BoardState) {
   const winningCombinations = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [9, 4, 6],
-    [3, 4, 6],
-    [0, 4, 6]
+    [0, 1, 2, 3],
+    [4, 5, 6 ,7],
+    [8, 9, 10, 11],
+    [12, 13, 14, 15],
+    [0, 5, 10, 15],
+    [3, 6, 9, 12],
+    [0, 4, 8, 12],
+    [1, 5, 9, 13],
+    [2, 6, 10, 14],
+    [3, 7, 11, 15] 
   ];
+
   for (let i = 0; i < winningCombinations.length; i++) {
     const [a, b, c, d] = winningCombinations[i];
-    if (boardState[a] && boardState[a] === boardState[b] && boardState[a] === boardState[c] && boardState[c] === boardState[d]) {
-      return boardState[a];
+    if (boardState[a] && boardState[a] === boardState[b] && boardState[a] === boardState[c] && boardState[d]) {
+      return boardState[a];  
     }
   }
   return null;
@@ -48,6 +49,7 @@ export function useGameState() {
     if (calculateWinner(boardState) || boardState[square]) {
       return;
     }
+
     const newBoardState = boardState.slice();
     newBoardState[square] = (gameState.step % 2) === 0 ? 'X' : 'O';
     history.push(newBoardState);
@@ -56,6 +58,7 @@ export function useGameState() {
       step: history.length - 1,
     });
   }
+  
   function jumpTo(step: number) {
     setGameState({
       history: gameState.history,
