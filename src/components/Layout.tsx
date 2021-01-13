3f4e94fda223d980f1326f8fad7f5f75ec941d0c
Preeti <preetijav@gmail.com>
2021-01-13 23:08:15 +0100

    css/ fix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,18 @@
 import styled from 'styled-components';
+
 type LayoutProps = {
   gap: number;
 };
 
 export const Row = styled.div<LayoutProps> `
-  display: flex;
   flex-direction: row;
   gap: ${(props) => props.gap}px;
+  display: flex;
 `;
 
 export const Column = styled.div<LayoutProps> `
   display: flex;
-  text-align: center;
+  text-align: left;
   flex-direction: column;
   gap: ${(props) => props.gap}px;
-  padding-left: 8rem;
 `;
